dcd3391104fecc57602b8c98510903e843abdbe9
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-05-10 16:52:38 +0300

    [Enh](UploadFile): Made url of an uploaded file public.
    Changed Access Control List to public read in S3Service.uploadFile.

--- src/main/kotlin/com/example/pudge/service/S3Service.kt
+++ src/main/kotlin/com/example/pudge/service/S3Service.kt
@@ -5,27 +5,24 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.http.SdkHttpResponse
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
 import software.amazon.awssdk.core.sync.RequestBody
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.*
 import java.util.*
 
 
 interface StorageService {
 
     // Bucket manipulations.
-    fun getBucketFileList(bucketName: String) : List<String>
+    fun getBucketFileList(bucketName: String): List<String>
 
     // File manipulations.
-    fun downloadFile(bucketName: String, fileKey: String) : ByteArray
+    fun downloadFile(bucketName: String, fileKey: String): ByteArray
 
-    fun getFileUrl(bucketName: String, fileKey: String) : String
+    fun getFileUrl(bucketName: String, fileKey: String): String
 
-    fun deleteFile(bucketName: String, fileKey: String) : SdkHttpResponse
+    fun deleteFile(bucketName: String, fileKey: String): SdkHttpResponse
 
-    fun uploadFile(bucketName: String, file: MultipartFile) : String
+    fun uploadFile(bucketName: String, file: MultipartFile): String
 
 }
 
@@ -39,31 +36,42 @@ class S3Service(var s3Client: S3Client) : StorageService {
     }
 
     @Async
-    override fun downloadFile(bucketName: String, fileKey: String) : ByteArray {
+    override fun downloadFile(bucketName: String, fileKey: String): ByteArray {
         return s3Client.getObject(
-            GetObjectRequest.builder().bucket(bucketName).key(fileKey).build()).readAllBytes()
+            GetObjectRequest.builder().bucket(bucketName).key(fileKey).build()
+        ).readAllBytes()
     }
 
     @Async
-    override fun getFileUrl(bucketName: String, fileKey: String) : String {
+    override fun getFileUrl(bucketName: String, fileKey: String): String {
         return s3Client.utilities().getUrl { builder -> builder.bucket(bucketName).key(fileKey) }
             .toExternalForm()
     }
 
-    override fun deleteFile(bucketName: String, fileKey: String) : SdkHttpResponse {
+    override fun deleteFile(bucketName: String, fileKey: String): SdkHttpResponse {
         return s3Client.deleteObject(
-            DeleteObjectRequest.builder().bucket(bucketName).key(fileKey).build()).sdkHttpResponse()
+            DeleteObjectRequest.builder().bucket(bucketName).key(fileKey).build()
+        ).sdkHttpResponse()
     }
 
     @Async
-    override fun uploadFile(bucketName: String, file: MultipartFile) : String {
+    override fun uploadFile(bucketName: String, file: MultipartFile): String {
+        // Should be unique in the bucket.
         val fileKey = file.name + UUID.randomUUID().toString()
-        val response = s3Client.putObject(PutObjectRequest.builder()
+
+        val acl: ObjectCannedACL = ObjectCannedACL.PUBLIC_READ
+        val putObjectRequest = PutObjectRequest.builder()
             .bucket(bucketName)
             .key(fileKey)
-            .build(),
+            // Access Control List.
+            .acl(acl)
+            .build()
+
+        val response = s3Client.putObject(
+            putObjectRequest,
             RequestBody.fromBytes(file.bytes)
-        ).sdkHttpResponse()
+        )
+            .sdkHttpResponse()
 
         return getFileUrl(bucketName, fileKey)
     }
