ecc4dbdf1b4045e62c4d4509e887b9993a3c4e65
DonVadimon <provcydia@gmail.com>
2022-05-19 22:57:18 +0300

    set aws

--- src/main/kotlin/com/example/pudge/configuration/S3Config.kt
+++ src/main/kotlin/com/example/pudge/configuration/S3Config.kt
@@ -1,25 +1,17 @@
 package com.example.pudge.configuration
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
-import java.time.Duration
 
 
 @Configuration
 class S3Config {
     @Bean(destroyMethod = "close")
-    fun s3Client() : S3Client {
+    fun s3Client(): S3Client {
         val regions: MutableList<Region> = Region.regions()
         val region = Region.EU_NORTH_1
-        return S3Client
-            .builder()
-            .region(region)
-            .build()
+        return S3Client.builder().region(region).build()
     }
 }
