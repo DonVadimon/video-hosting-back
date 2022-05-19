3f9bc20edc09e5a4abebb673ec775ac102d1e66f
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-05-19 22:31:01 +0300

    Storage (#2)
    * [Enh](Init): First blood.
    
    * [Enh](BucketFileList): Implemented S3Config, S3Service with BucketFileList handle.
    
    * [Enh](DownloadFile&DeleteFile): Implemented downloading and deleting files.
    
    * [Enh](UploadFile): Added file upload to storage service and controller.
    
    * [Enh](GetFileUrl): Added getFileUrl to storage service and controller.
    
    * [Enh](UploadFile): Added url of a newly uploaded object to the response.
    
    * [Enh](UploadFile): Made url of an uploaded file public.
    
    Changed Access Control List to public read in S3Service.uploadFile.
    
    * [Fix](StorageRoles): Organized roles properly.
    
    * Ordinary users can read:
    - getFileList,
    - download,
    - getFileUrl.
    
    * Admin and VideoCreator can *also* write:
    - deleteFile,
    - uploadFile
    
    * [Fix](FileUploadExtension): Added extension to the end of the object key.
    
    * [Conf](FileUploadSize): Moved file size limit up to 200MB.

---
+++ src/main/kotlin/com/example/pudge/api/StorageApi.kt
@@ -0,0 +1,62 @@
+package com.example.pudge.api
+
+import com.example.pudge.domain.entity.ConstAuthorities
+import com.example.pudge.service.StorageService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
+import javax.annotation.security.RolesAllowed
+
+@RestController
+@RequestMapping(path = ["storage"])
+class StorageApi(
+    private var storageService: StorageService
+) {
+
+    @GetMapping("{bucket}")
+    fun getFileList(
+        @PathVariable("bucket") bucket: String
+    ): ResponseEntity<List<String>> {
+        return ResponseEntity.of(Optional.of(storageService.getBucketFileList(bucket)))
+    }
+
+    @DeleteMapping("{bucket}/{file}")
+    @RolesAllowed(ConstAuthorities.ADMIN.name, ConstAuthorities.VIDEO_CREATOR.name)
+    fun deleteFile(
+        @PathVariable("bucket") bucket: String,
+        @PathVariable("file") fileKey: String): ResponseEntity<Any?> {
+        val responseEntity = storageService.deleteFile(bucket, fileKey)
+        return ResponseEntity(responseEntity, HttpStatus.valueOf(responseEntity.statusCode()))
+    }
+
+    @GetMapping("{bucket}/{file}")
+    fun downloadFile(
+        @PathVariable("bucket") bucket: String,
+        @PathVariable("file") fileKey: String): ResponseEntity<ByteArray> {
+        val sourceFile : ByteArray = storageService.downloadFile(bucket, fileKey)
+        return ResponseEntity.ok().contentLength(sourceFile.size.toLong())
+            .header("Content-type", "application/octet-stream")
+            .header("Content-disposition", "attachment; filename=\"$fileKey\"")
+            .body(sourceFile)
+    }
+
+    @GetMapping("{bucket}/{file}/url")
+    fun getFileUrl(
+        @PathVariable("bucket") bucket: String,
+        @PathVariable("file") fileKey: String): ResponseEntity<String> {
+        return ResponseEntity.of(Optional.of(storageService.getFileUrl(bucket, fileKey)))
+    }
+
+    @PostMapping("{bucket}")
+    @RolesAllowed(ConstAuthorities.ADMIN.name, ConstAuthorities.VIDEO_CREATOR.name)
+    fun uploadFile(
+        @PathVariable("bucket") bucket: String,
+        @RequestPart("file") file : MultipartFile
+    ): ResponseEntity<Any?> {
+        val awsResponse = storageService.uploadFile(bucket, file)
+        return ResponseEntity.ok(awsResponse)
+    }
+
+}
