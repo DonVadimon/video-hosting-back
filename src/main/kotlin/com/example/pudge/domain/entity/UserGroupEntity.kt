23282eb592a64936ab38819c63a2adce4867a2ac
DonVadimon <provcydia@gmail.com>
2022-05-20 23:23:48 +0300

    videos for groups

--- src/main/kotlin/com/example/pudge/domain/entity/UserGroupEntity.kt
+++ src/main/kotlin/com/example/pudge/domain/entity/UserGroupEntity.kt
@@ -5,9 +5,15 @@ import javax.persistence.*
 @Entity
 @Table(name = "user_group_entity")
 class UserGroupEntity(
-    @Column(length = 100, unique = true) val name: String, @OneToMany(
+    @Column(length = 100, unique = true) val name: String,
+    @OneToMany(
         mappedBy = "group", fetch = FetchType.LAZY, orphanRemoval = true, cascade = [CascadeType.ALL]
-    ) val users: MutableList<UserEntity> = mutableListOf()
+    ) val users: MutableList<UserEntity> = mutableListOf(),
+    @ManyToMany(fetch = FetchType.EAGER) @JoinTable(
+        name = "video_groups",
+        joinColumns = [JoinColumn(name = "group_id")],
+        inverseJoinColumns = [JoinColumn(name = "video_id")]
+    ) val videos: MutableSet<VideoEntity>? = HashSet(),
 ) : BaseEntity<Long>() {
     fun addUser(block: UserGroupEntity.() -> UserEntity) {
         users.add(block())
